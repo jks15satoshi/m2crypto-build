81bbdfe990ac1a2150e3e10486c7cc82af5fe90d
Satoshi Jek <jks15satoshi@gmail.com>
2021-07-08 19:14:53 +0800

    improv: Add warning message printing.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # m2crypto-build
+
+Build
+
+## References
+
+[Comment on iOSForensics/pymobiledevice #25](https://github.com/iOSForensics/pymobiledevice/issues/25#issuecomment-576119104) by [@tech234a](https://github.com/tech234a).
--- build.ps1
+++ build.ps1
@@ -5,9 +5,24 @@
 # License: MIT License, see LICENSE for details.
 
 # Definitions for formatted outputs
-function ErrorOutput {
+function Format-Error {
     process { Write-Host $_ -ForegroundColor Red }    
 }
+function Format-Warning {
+    process { Write-Host $_ -ForegroundColor Yellow }
+}
+
+Clear-Host
+
+# WARNING MESSAGE
+$warn_msg = @'
+CAUTION:
+This script will download or install programmes or files to your device. After processing, you will be prompted to clean up the unnecessary files or reserve them.
+This script cannot grantee that will not destroy your device, so DO NOT RUN this script unless you completely understand what the intention of this script is.
+You can now press Ctrl+C to stop the process if you cannot trust this script or have no idea on how to handle the possible problems.
+'@
+Write-Output $warn_msg | Format-Warning
+Read-Host -Prompt 'Press Enter to continue, or Ctrl+C to stop'
 
 # Get system architecture information
 $arch_version = if ([System.Environment]::Is64BitOperatingSystem) { '64' } else { '32' }
@@ -19,11 +34,12 @@ Write-Output 'Downloading OpenSSL...'
 try {
     $wc = New-Object System.Net.WebClient
     $wc.DownloadFile($url, 'OpenSSL.msi')
-} catch [System.Net.WebException],[System.IO.IOException] {
-    Write-Output 'Unable to download OpenSSL installer, script terminated.' | ErrorOutput
+}
+catch [System.Net.WebException], [System.IO.IOException] {
+    Write-Output 'Unable to download OpenSSL.msi file, script terminated.' | Format-Error
     exit 1
 }
 
 Write-Output 'Installing OpenSSL...'
-msiexec.exe /package /quiet .\OpenSSL.msi
+msiexec.exe / package / quiet .\OpenSSL.msi
 # msiexec.exe /uninstall /quiet '.\OpenSSL.msi'
