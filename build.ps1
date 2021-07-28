807d4a991b032f304da936b014204ce639ece1e3
Satoshi Jek <jks15satoshi@gmail.com>
2021-07-28 14:12:11 +0800

    feat: Add processes and some minor changes.
    - Restrict to execute this script on administrator mode only;
    - Install Python dependencies globally or in the indicated virtualenv;
    - Clone M2Crypto to local;
    - Build, generate and install M2Crypto;
    - The number of characters in a single line will not be limited to 80;
    - Some minor changes on output.

--- build.ps1
+++ build.ps1
@@ -12,16 +12,34 @@ param (
     [string] $PythonEnv,
     # Specified version of M2Crypto.
     [string] $ModuleVersion,
-    # Path to OpenSSL local installer. If exists, online downloading will be 
-    # skipped.
+    # Path to OpenSSL local installer. If exists, online downloading will be skipped.
     [string] $OpenSSL,
-    [string[]] $BuildType = ('whl', 'exe', 'msi'),
+    # Build and generate M2Crypto installable files only.
     [switch] $BuildOnly,
     # Skip to show warning messages.
     [switch] $NoWarnings,
     # Alias to Get-Help.
     [switch] $Help
 )
+
+Import-Module BitsTransfer
+
+Clear-Host
+
+function Write-Info {
+    <#
+    .SYNOPSIS
+        Output message with the foreground color as cyan.
+    
+    .PARAMETER Message
+        The output message.
+    #>
+    param (
+        [string] $Message
+    )
+
+    Write-Host ("`n>>> " + $Message) -ForegroundColor Cyan
+}
 
 function Write-Error {
     <#
@@ -35,7 +53,7 @@ function Write-Error {
         [string] $Message
     )
 
-    Write-Host $Message -ForegroundColor Red
+    Write-Host ("ERROR: " + $Message) -ForegroundColor Red -BackgroundColor Black
     exit 1
 }
 
@@ -51,18 +69,18 @@ function Install-OpenSSL {
         [string] $Path
     )
 
-    Write-Output 'Installing OpenSSL...'
+    Write-Info "Installing OpenSSL..."
     Start-Process msiexec.exe -Wait -ArgumentList (
-        '/package {0}' -f $(Convert-Path $Path), '/passive', '/le err.log')
+        "/package " + $(Convert-Path $Path), "/passive", "/le err.log")
     
     try {
         [string] $log = Get-Content .\err.log -ErrorAction Stop
     }
     catch [System.Management.Automation.ItemNotFoundException] {
-        Write-Error 'Failed to install OpenSSL, script terminated.'
+        Write-Error "Failed to install OpenSSL, script terminated."
     }
-    if (!$log -or $log -like '*Error*') {
-        Write-Error 'Failed to install OpenSSL, script terminated.'
+    if (!$log -or $log -like "*Error*") {
+        Write-Error "Failed to install OpenSSL, script terminated."
     }
 }
 
@@ -72,26 +90,20 @@ if ($Help) {
     exit
 }
 
-# Warning message, use '-NoWarnings' to skip.
+# Warning message, use "-NoWarnings" to skip.
 if (!${NoWarnings}) {
     $warn_msg = @"
-`nThis script will download or install programmes or files to your device. After
-processing, you will be prompted to clean up the unnecessary files or reserve 
-them.
-This script cannot grantee that will not destroy your device, so DO NOT RUN this
-script unless you completely understand what the intention of this script is.
-You can now press Ctrl+C to stop the process if you cannot trust this script or 
-have no idea on how to handle the possible problems.
+`nThis script will download or install programmes or files to your device. After processing, you will be prompted to clean up the unnecessary files or reserve them.
+This script cannot grantee that will not destroy your device, so DO NOT RUN this script unless you completely understand what the intention of this script is.
+You can now press Ctrl+C to stop the process if you cannot trust this script or have no idea on how to handle the possible problems.
 "@
     Write-Warning $warn_msg
-    Read-Host -Prompt 'Press Enter to continue, or Ctrl+C to stop'
+    Read-Host -Prompt "Press Enter to continue, or Ctrl+C to stop"
 }
-
-Clear-Host
 
 # Step 1: Install OpenSSL.
 if ($OpenSSL) {
-    if ($OpenSSL -ne 'skip') {
+    if ($OpenSSL -ne "skip") {
         # Install OpenSSL from the indicated path.
         Install-OpenSSL -Path $OpenSSL
     }
@@ -99,42 +111,62 @@ if ($OpenSSL) {
 else {
     # Download and install OpenSSL v1.1.1k for the current system architecture.
     $is_x64 = [System.Environment]::Is64BitOperatingSystem
-    $arch = if ($is_x64) { '64' } else { '32' }
-    $url = 'https://slproweb.com/download/Win{0}OpenSSL-1_1_1k.msi' -f $arch
+    $arch = if ($is_x64) { "64" } else { "32" }
+    $url = "https://slproweb.com/download/Win" + $arch + "OpenSSL-1_1_1k.msi"
 
-    Write-Output 'Downloading OpenSSL...'
+    Write-Info "Downloading OpenSSL..."
     try {
-        $wc = New-Object System.Net.WebClient
-        $wc.DownloadFile($url, 'OpenSSL.msi')
+        Start-BitsTransfer $url "OpenSSL.msi" -Description "Downloading OpenSSL.msi." -ErrorAction Stop
     }
-    catch [System.Net.WebException], [System.IO.IOException] {
-        $err_msg = 'Unable to download OpenSSL.msi file, script terminated.'
-        Write-Error $err_msg
-        exit 1
+    catch {
+        Write-Error "Unable to download OpenSSL.msi file, script terminated."
     }
-    # Install downloaded OpenSSL.
-    Install-OpenSSL -Path .\OpenSSL.msi
+
+    Install-OpenSSL OpenSSL.msi
 }
 
 # Step 2: Get swig via Chocolatey.
-Write-Output "`nInstalling Chocolatey..."
+Write-Info "Installing Chocolatey..."
 Set-ExecutionPolicy Bypass -Scope Process -Force
-[System.Net.ServicePointManager]::SecurityProtocol = (
-    [System.Net.ServicePointManager]::SecurityProtocol -bor 3072)
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString(
-        'https://chocolatey.org/install.ps1'))
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString("https://chocolatey.org/install.ps1"))
 
-Write-Output 'Installing swig via Chocolatey...'
+Write-Info "Installing swig via Chocolatey..."
 choco install -r -y swig
 
 # Step 3: Install Python dependencies.
-Write-Output "`nInstalling Python dependencies..."
+Write-Info "Installing Python dependencies..."
 if ($PythonEnv) {
-    Invoke-Expression ('{0}Scripts\Activate.ps1' -f $(Convert-Path $PythonEnv))
-    Write-Output 'Python virtual environment has been activated.'
+    try {
+        Invoke-Expression ($(Convert-Path $PythonEnv) + "Scripts\Activate.ps1")
+    }
+    catch {
+        Write-Error "The indicated path is not a valid Python virtual environment."
+    }
+    
+    Write-Info "Python virtual environment has been activated."
 }
 
 pip install pywin32 wheel
 if ( -not $?) {
-    Write-Error 'Error occurred while installing dependencies from pip.'
+    Write-Error "Error occurred while installing dependencies from pip."
+}
+
+# Step 4: Clone M2Crypto from GitLab
+Write-Info "Cloning M2Crypto to local..."
+
+if (!(Test-Path '.\m2crypto')) {
+    [void](New-Item -Path . -Name "m2crypto" -ItemType "directory")
+}
+Set-Location .\m2crypto
+git clone "https://gitlab.com/m2crypto/m2crypto" .
+
+# Step 5: Build and generate M2Crypto installable files.
+python setup.py build --openssl="C:\Program Files\OpenSSL-Win64" --bundledlls
+python setup.py bdist_wheel bdist_wininst bdist_msi
+
+# Step 6: Install M2Crypto to the currently activated Python virtual environment.
+if (!$BuildOnly) {
+    Set-Location .\dist
+    pip install ([string](Get-ChildItem . "*.whl" -Name))
 }
